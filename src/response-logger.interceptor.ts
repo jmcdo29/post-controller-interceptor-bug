6e435c8ad40854b77ed9e9fe66663f849a909804
Jay McDoniel <jmcdo29@gmail.com>
2022-02-02 13:22:42 -0800

    feat: initial commit for bug reproduction

---
+++ src/response-logger.interceptor.ts
@@ -0,0 +1,18 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, tap } from 'rxjs';
+
+@Injectable()
+export class ResponseLoggerInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      tap((data) => {
+        console.log(JSON.stringify(data));
+      }),
+    );
+  }
+}
